45b848228fd57de57bbafd1a24618f480cb33ad3
Walany Costa <walanyBnegro@gmail.com>
2021-12-30 12:27:20 +0000

    ensure than verifyWinner determines the result games

--- src/page/mainScreen.jsx
+++ src/page/mainScreen.jsx
@@ -6,7 +6,48 @@ const MainScreen = (props) => {
     const [showLetters, setShowLetters] = useState(new Map());
     const items = ['item-1','item-2','item-3','item-4','item-5','item-6','item-7','item-8','item-9'];
 
-    const writeLetter = (id, letter) =>{
+
+    const verifyWinner = (map) =>{
+        if(map.size > 0){
+            if(map.get('item-1') === map.get('item-2') && map.get('item-2') === map.get('item-3')
+                && map.has('item-2')){
+                    console.log('Vencedor')
+                    setShowLetters(new Map()) 
+            }else if(map.get('item-4') === map.get('item-5') && map.get('item-5') === map.get('item-6')
+                && map.has('item-5')){
+                    console.log('Vencedor')
+                    setShowLetters(new Map()) 
+            }else if(map.get('item-7') === map.get('item-8') && map.get('item-8') === map.get('item-9') 
+                && map.has('item-8')){
+                    console.log('Vencedor')
+                    setShowLetters(new Map()) 
+            }else if(map.get('item-1') === map.get('item-4') && map.get('item-4') === map.get('item-7') 
+                && map.has('item-4')){
+                    console.log('Vencedor')
+                    setShowLetters(new Map()) 
+            }else if(map.get('item-2') === map.get('item-5') && map.get('item-5') === map.get('item-8') 
+                && map.has('item-5')){
+                    console.log('Vencedor')
+                    setShowLetters(new Map()) 
+            }else if(map.get('item-3') === map.get('item-6') && map.get('item-6') === map.get('item-9') 
+                && map.has('item-6')){
+                    console.log('Vencedor')
+                    setShowLetters(new Map()) 
+            }else if(map.get('item-1') === map.get('item-5') && map.get('item-5') === map.get('item-9') 
+                && map.has('item-5')){
+                    console.log('Vencedor')
+                    setShowLetters(new Map()) 
+            }else if(map.get('item-3') === map.get('item-5') && map.get('item-5') === map.get('item-7') 
+                && map.has('item-5')){
+                    console.log('Vencedor')
+                    setShowLetters(new Map()) 
+            }else if(map.size === 9){
+                console.log('Empate');
+                setShowLetters(new Map())
+            }
+    }}
+
+    const writeLetter = (id) =>{
         const map = new Map();
         if(showLetters.size !== 0){
             for(let value of showLetters.entries()){
@@ -14,9 +55,10 @@ const MainScreen = (props) => {
                 map.set(olId, oldLetter);
             }
         } 
-        
+  
         map.set(id, changeLetter());
         setShowLetters(map);
+        verifyWinner(map);
     }
 
     return (
@@ -26,7 +68,7 @@ const MainScreen = (props) => {
                     return (
                         <div key={item} onClick={(e)=>{
                             e.preventDefault();
-                            writeLetter(item, 'o');
+                            writeLetter(item);
                         }}>
                             {showLetters.get(item)}
                         </div>
